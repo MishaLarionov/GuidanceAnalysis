e4bf3368c57a254cd050cf897fdd13feb004b3ad
Misha Larionov <MishaLarionov@users.noreply.github.com>
2017-11-17 10:50:24 -0500

    Added custom implementation of JSON writing

---
+++ src/JSON.java
@@ -0,0 +1,60 @@
+/**
+ * JSON.java
+ * Writes a HashMap of String keys and ArrayList<String> values to a JSON string
+ * Misha Larionov
+ */
+
+//Import statements
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+//Start the class
+public class JSON {
+    //It's all static because I don't benefit much from instantiating it
+    public static String toJSONString(HashMap<String, ArrayList<String>> map, int indentationLevel) {
+        StringBuilder outputJSON = new StringBuilder();
+        outputJSON.append(String.join("", Collections.nCopies(indentationLevel, "   "))); //Add some tabs
+        outputJSON.append("{\n");
+
+        for (String key : map.keySet()) {
+            outputJSON.append(String.join("", Collections.nCopies(indentationLevel + 1, "   ")));
+            outputJSON.append("\"" + key + "\" :\n");
+            outputJSON.append(toJSONString(map.get(key), indentationLevel + 1));
+            outputJSON.append(",\n");
+        }
+        //Delete trailing comma (Second-to-last element since we have a newline)
+        outputJSON.deleteCharAt(outputJSON.length() - 2);
+
+        outputJSON.append(String.join("", Collections.nCopies(indentationLevel, "   ")));
+        outputJSON.append("}");
+        return outputJSON.toString();
+    }
+
+    public static String toJSONString(HashMap<String, ArrayList<String>> map) {
+        return toJSONString(map, 0);
+    }
+
+    public static String toJSONString(ArrayList<String> list, int indentationLevel) {
+        StringBuilder outputJSON = new StringBuilder();
+        outputJSON.append(String.join("", Collections.nCopies(indentationLevel, "   ")));
+        outputJSON.append("[\n");
+
+        for (String value : list) {
+            outputJSON.append(String.join("", Collections.nCopies(indentationLevel + 1, "   ")));
+            outputJSON.append("\"" + value + "\",\n");
+        }
+
+        //Delete trailing comma (Second-to-last element since we have a newline)
+        outputJSON.deleteCharAt(outputJSON.length() - 2);
+
+        //Output the final bracket and return
+        outputJSON.append(String.join("", Collections.nCopies(indentationLevel, "   ")));
+        outputJSON.append("]");
+        return outputJSON.toString();
+    }
+
+    public static String toJSONString(ArrayList<String> list) {
+        return toJSONString(list, 0);
+    }
+}
--- src/Tagger.java
+++ src/Tagger.java
@@ -4,6 +4,7 @@
  */
 
 //Import statements
+import java.io.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,15 +14,51 @@ public class Tagger {
 	//Matching of course codes to tags
 	HashMap<String, ArrayList<String>> tagDatabase;
 	//List of possible tags for future implementation
-	ArrayList<String> possibleTags;
+	private ArrayList<String> possibleTags;
+	private Scanner inputScanner;
 	
 	Tagger() {
-		//todo: Read from file for persistence
-		tagDatabase = new HashMap<>();
-		possibleTags = new ArrayList<>();
+        inputScanner = new Scanner(System.in);
+	    try {
+            BufferedReader input = new BufferedReader(new FileReader("res/tags.json"));
+            StringBuilder JSONString = new StringBuilder();
+            String nextLine = input.readLine();
+            while (nextLine != null) {
+                JSONString.append(nextLine);
+                nextLine = input.readLine();
+            }
+            //todo delet this once writing works
+            tagDatabase = new HashMap<>();
+            possibleTags = new ArrayList<>();
+        } catch (FileNotFoundException E) {
+	        //This exception happens if the file doesn't exist
+            tagDatabase = new HashMap<>();
+            possibleTags = new ArrayList<>();
+        } catch (IOException E) {
+	        System.out.println("Exception while fetching tags: " + E);
+        }
 	}
+
+	private boolean saveTags() {
+	    //Write all the data to JSON
+	    StringBuilder JSONString = new StringBuilder("[\n");
+	    JSONString.append(JSON.toJSONString(this.tagDatabase, 1));
+	    JSONString.append(",\n");
+	    JSONString.append(JSON.toJSONString(this.possibleTags, 1));
+	    JSONString.append("\n]");
+	    try {
+            BufferedWriter output = new BufferedWriter(new FileWriter("res/tags.json"));
+            output.write(JSONString.toString());
+            output.close();
+        } catch (IOException E) {
+	        System.out.println("Exception while saving tags: " + E);
+	        return false;
+        }
+
+        return true;
+    }
 	
-	public ArrayList<String> GetTags(String programCode, String programName) {
+	public ArrayList<String> getTags(String programCode, String programName) {
 		if (tagDatabase.containsKey(programCode)) {
 			//The course code already has tags associated with it! Just return those
 			return tagDatabase.get(programCode);
@@ -29,14 +66,30 @@ public class Tagger {
 			//Prompt the user for tags
 			//TODO: Make this a GUI
 			//Scanner is temporary, only for testing
-			Scanner input = new Scanner(System.in);
-			String newTagString = input.nextLine();
-			input.close();
+			String newTagString = inputScanner.nextLine();
 			ArrayList<String> newTags = new ArrayList<String>(Arrays.asList(newTagString.split(",")));
 			//End temporary testing code
-			possibleTags.addAll(newTags);
+
+            //Add new elements to possibleTags while keeping out duplicates
+            for (String tag : newTags) {
+                boolean unique = true;
+                for (String oldTag : possibleTags) {
+                    if (oldTag.equals(tag)) {
+                        unique = false;
+                    }
+                }
+                if (unique) {
+                    possibleTags.add(tag);
+                }
+            }
 			tagDatabase.put(programCode, newTags);
+			this.saveTags();
 			return newTags;
 		}
+	}
+
+	public ArrayList<String> editTags(String programCode, String programName) {
+		//TODO: Call the GUI
+		return this.getTags(programCode, programName);
 	}
 }
