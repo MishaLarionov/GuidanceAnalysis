5a54f0df38da73b87a6f6f0730aabb55bad4b7ed
Misha Larionov <MishaLarionov@users.noreply.github.com>
2017-11-21 09:03:29 -0500

    Updated the file chooser

--- src/FileChooser.java
+++ src/FileChooser.java
@@ -8,37 +8,35 @@
 import java.io.*;
 import javax.swing.JFileChooser;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.awt.event.ActionEvent;
 
 public class FileChooser extends JPanel{
 	
     JFileChooser fc;
     
-    public FileChooser(){
+    FileChooser(){
     	super(new BorderLayout());
-    	
-    	fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-    	fc.setDialogTitle("Choose a university file:");
-    	
-    	fc.setAcceptAllFileFilterUsed(false);
-    	FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV files", "csv");
-    	fc.addChoosableFileFilter(filter);
-    	//fc.setFileSelectionMode(JFileChooser.FILES_ONLY); //already the default mode  	    	
-    	
-    	int returnValue = fc.showSaveDialog(null);
-    	if (returnValue == JFileChooser.APPROVE_OPTION){
-    		File file = fc.getSelectedFile();
-    		System.out.println("You selected the directory: " + file);
-    		
-    	}
-    	
+    }
+
+    public File getFile() {
+        fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        fc.setDialogTitle("Choose a university file:");
+
+        fc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV files", "csv");
+        fc.addChoosableFileFilter(filter);
+        //fc.setFileSelectionMode(JFileChooser.FILES_ONLY); //already the default mode
+
+        int returnValue = fc.showSaveDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION){
+            File file = fc.getSelectedFile();
+            return file;
+        }
+
+        return null;
+
     }
     
 
