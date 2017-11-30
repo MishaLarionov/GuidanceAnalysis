f1019375897e1a71653676416da9eb606c5efc2a
Misha Larionov <MishaLarionov@users.noreply.github.com>
2017-11-30 02:40:58 -0500

    Added description of the problem so I can come back tomorrow.

--- src/AddTags.java
+++ src/AddTags.java
@@ -82,7 +82,10 @@ public class AddTags extends JFrame implements ListSelectionListener {
     tagField.addActionListener(addListener);
     tagField.getDocument().addDocumentListener(addListener);
     String thisTag = list.getSelectedIndex() > -1 ? listModel.getElementAt(list.getSelectedIndex()).toString() : null ;
-    
+
+    //TODO: NOTHING BEYOND THIS LINE WORKS
+    //It's not displaying even though I saw it working a while ago
+
     //finish button
     JButton finishButton = new JButton("Save and Finish");
     finishButton.addActionListener(new FinishListener());
